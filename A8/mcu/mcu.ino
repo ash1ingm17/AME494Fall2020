95f83bc369073aef151176c79d705fdf8fef40af
Ashling M <>
2020-11-24 14:56:09 -0700

    A8

--- A8/mcu/mcu.ino
+++ A8/mcu/mcu.ino
@@ -2,6 +2,7 @@
 #include <LilyGoWatch.h>
 
 TTGOClass *ttgo;
+#include <SimpleDHT.h>
 
 #include <WiFi.h>
 #include <HTTPClient.h>
@@ -10,10 +11,25 @@ const char* ssid = "MSMAMM";
 const char* password = "donegalireland";
 
 //Your Domain name with URL path or IP address with path
-const char* serverName = "http://34.205.65.193:1234/sendEmail";
+const char* serverName = "http://34.205.65.193:1234/sendData";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 300;
 
 String response;
 
+
+// for DHT11,
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 25
+int pinDHT11 = 25;
+SimpleDHT11 dht11(pinDHT11);
 
 String httpGETRequest(const char* serverName) {
   HTTPClient http;
@@ -47,13 +63,30 @@ void pressed()
     ttgo->tft->fillScreen(color);
     ttgo->tft->setTextColor(color, TFT_WHITE);
     ttgo->tft->drawString("User Button pressed",  5, 100, 4);
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
 
+    int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err); delay(1000);
+    return;
+  }
+
+  Serial.print("Sample OK: ");
+  Serial.print(String((float)temperature) + "* C, ");
+  Serial.println(String((float)humidity) + "% H");
+
+   int t = (int)temperature;
+      int h = (int)humidity;
+      String url = String(serverName) + "?t=" + t + "&h=" + h;
+      Serial.println(url);   
+
+  
     if(WiFi.status()== WL_CONNECTED){
-              
-      response = httpGETRequest(serverName);
+     response = httpGETRequest(url.c_str());
       Serial.println(response);
-
-    }
+}
     else {
       Serial.println("WiFi Disconnected");
     }
---
+++ A8/server/server.js
@@ -0,0 +1,60 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ame494test@gmail.com',
+    pass: 'Testing123#' // naturally, replace both with your real credentials or an application-specific password
+  }
+});
+
+const mailOptions = {
+  from: 'ame494test@gmail.com',
+  to: 'ame494test@gmail.com',
+  subject: 'Temperature and Humidity'
+  text: 'testing.'
+};
+
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+app.get("/", function (req, res) {
+    res.redirect("index.html")
+});
+
+app.get("/sendEmail", function (req, res) {
+   // Send Email
+   t - req.query.t;
+   h = req.query.h;
+   req.query.time = new Date().getTime();
+
+   transporter.sendMail({
+     from: 'ame494test@gmail.com',
+     to: 'ame494test@gmail.com',
+     subject: 'Temperature and Humidity'
+     text: 'Temperature is ' + t +' and Humidity is ' + h +'.'
+   }
+
+, function(error, info){
+     if (error) {
+   	console.log(error);
+     } else {
+       console.log('Email sent: ' + info.response);
+     }
+   });
+});
+
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
